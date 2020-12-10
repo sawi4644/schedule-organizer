3f9e021f10d15e28ad6d5c2347cddf42b93b48fd
Sara Wilson <sarakatewilson98@gmail.com>
2020-12-09 20:04:53 -0500

    copying previous function to implement to other IDs

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-//global variables
+// global variables
 var currentDay= moment().format('LL'); 
 
-//updating current day to html
+// updating current day to html
 $('#currentDay').text(currentDay)
 
+// add event listener click for button
+// saving user input to local storage
+$('#nineBtn').on('click',function(e){
+    var userInput= $('#nineText').val();
+    localStorage.setItem('nine', userInput);
+})
+// getting stored value and displaying
+$('#nineText').val(localStorage.getItem('nine'));
+
+$('#tenBtn').on('click',function(e){
+    var userInput= $('#tenText').val();
+    localStorage.setItem('ten', userInput);
+})
+// getting stored value and displaying
+$('#tenText').val(localStorage.getItem('ten'));
+
+// grab text user input information from correct row
+// save to local storage
+// display new information on correct row
 
 
 
 
 
-//input information into text area
-//information saves to local storage
+
 
 
 // WHEN I view the time blocks for that day
