68c86531280367af33c7721674c7bf86ee16cdfe
Sara Wilson <sarakatewilson98@gmail.com>
2020-12-09 19:05:10 -0500

    pseduo code

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 //global variables
-var currentDay= $('#currentDay')
+var currentDay= moment().format('LL'); 
 
 //updating current day to html
+$('#currentDay').text(currentDay)
 
-currentmoment().format('MMMM Do YYYY, h:mm:ss a');
+
+
+
+
+
+//input information into text area
+//information saves to local storage
+
+
+// WHEN I view the time blocks for that day
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+// WHEN I click into a time block
+// THEN I can enter an event
+// WHEN I click the save button for that time block
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
