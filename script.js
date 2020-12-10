01edca05c3c76b897af278e133f4862b0c78f4be
Sara Wilson <sarakatewilson98@gmail.com>
2020-12-09 20:32:26 -0500

    changing present color to purple

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // global variables
-var currentDay= moment().format('LL'); 
+var currentDay= moment().format('LL');
+var currentTime = moment().format('LT');
 
 // updating current day to html
 $('#currentDay').text(currentDay)
+// updating current time to index
+$('#currentTime').text(currentTime)
 
 // add event listener click for button
 // saving user input to local storage
 $('#nineBtn').on('click',function(e){
     var userInput= $('#nineText').val();
+    e.preventDefault();
     localStorage.setItem('nine', userInput);
 })
 // getting stored value and displaying
@@ -16,14 +20,12 @@ $('#nineText').val(localStorage.getItem('nine'));
 // working function for text storage at ten
 $('#tenBtn').on('click',function(e){
     var userInput= $('#tenText').val();
+    e.preventDefault();
     localStorage.setItem('ten', userInput);
 })
 $('#tenText').val(localStorage.getItem('ten'));
 
-
-
-
-
+// color changing blocks based on time
 
 
 // WHEN I view the time blocks for that day
