4645ca3362f2e9c1bf7174ec5182bf87399b5e20
Sara Wilson <sarakatewilson98@gmail.com>
2020-12-09 21:25:14 -0500

    adding more local storage functions to other time slots

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $('#currentTime').text(currentTime)
 
 // add event listener click for button
 // saving user input to local storage
+// Event listener to save to local stroage
 $('#nineBtn').on('click',function(e){
     var userInput= $('#nineText').val();
     e.preventDefault();
@@ -25,7 +26,31 @@ $('#tenBtn').on('click',function(e){
 })
 $('#tenText').val(localStorage.getItem('ten'));
 
+// working function for text storage at eleven
+$('#elevenBtn').on('click',function(e){
+    var userInput= $('#elevenText').val();
+    e.preventDefault();
+    localStorage.setItem('eleven', userInput);
+})
+$('#elevenText').val(localStorage.getItem('eleven'));
+// working function for text storage at twelve
+$('#twelveBtn').on('click',function(e){
+    var userInput= $('#twelveText').val();
+    e.preventDefault();
+    localStorage.setItem('twelve', userInput);
+})
+$('#twelveText').val(localStorage.getItem('twelve'));
+// working function for text storage at one
+$('#oneBtn').on('click',function(e){
+    var userInput= $('#oneText').val();
+    e.preventDefault();
+    localStorage.setItem('one', userInput);
+})
+$('#oneText').val(localStorage.getItem('one'));
 // color changing blocks based on time
+// if time is current then .present
+// if time is past then .past
+// if time is future then .past
 
 
 // WHEN I view the time blocks for that day
